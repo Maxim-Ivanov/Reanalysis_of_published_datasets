cf9d85ef343f1e2d945beba2e4fea6ba1b2b9930
Maxim-Ivanov <35961244+Maxim-Ivanov@users.noreply.github.com>
2021-03-11 11:40:20 +0100

    Update convert_RNAseq_BAM_to_normalized_bedGraph.R

--- RNA-seq/convert_RNAseq_BAM_to_normalized_bedGraph.R
+++ RNA-seq/convert_RNAseq_BAM_to_normalized_bedGraph.R
@@ -9,7 +9,7 @@ paste0(prefix, "save_GRanges_as_bedGraph.R?raw=TRUE") %>% devtools::source_url()
 bamdir <- "."
 bamfiles <- list.files(pattern = "bam$")
 
-RNA-seq_BAM_to_normalized_bedGraph <- function(bamfile, mode = "PE", stranded = TRUE, switch_strand = TRUE, normalize = TRUE, norm_to = 1e06) {
+RNA_seq_BAM_to_normalized_bedGraph <- function(bamfile, mode = "PE", stranded = TRUE, switch_strand = TRUE, normalize = TRUE, norm_to = 1e06) {
   stopifnot(mode %in% c("PE", "SE"))
   if (mode == "PE") {
     ga <- readGAlignmentPairs(bamfile)
@@ -21,10 +21,10 @@ RNA-seq_BAM_to_normalized_bedGraph <- function(bamfile, mode = "PE", stranded = 
 }
 
 # For stranded PE data:
-cov_list <- lapply(bamfiles, RNA-seq_BAM_to_normalized_bedGraph)
+cov_list <- lapply(bamfiles, RNA_seq_BAM_to_normalized_bedGraph)
 
 # For stranded SE data (e.g. Kohnen et al., 2016 - PMID 27923878):
-cov_list <- lapply(bamfiles, RNA-seq_BAM_to_normalized_bedGraph, mode = "SE")
+cov_list <- lapply(bamfiles, RNA_seq_BAM_to_normalized_bedGraph, mode = "SE")
 
 # Save as bedGraph files:
 names(cov_list) <- bamfiles %>% basename() %>% str_replace(".bam")
